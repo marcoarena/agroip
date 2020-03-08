23c062b85a92327beafff8ccc4f6e1bd7e0f597c
marcoarena <53092985+marcoarena@users.noreply.github.com>
2020-03-08 23:20:47 +0100

    Add files via upload

---
+++ js/firecnfg.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDWCEOU324y5NVFKrOQuxw-EgGYx-a8bF0",
+    authDomain: "iginuu.firebaseapp.com",
+    databaseURL: "https://iginuu.firebaseio.com",
+    projectId: "iginuu",
+    storageBucket: "iginuu.appspot.com",
+    messagingSenderId: "346974463179",
+    appId: "1:346974463179:web:ec6ef14a7b99aba569a74e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
